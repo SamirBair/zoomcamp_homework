7e2dccbe57ba166ba830e084afd5d725f534b6d8
SamirBair <s.bartels89@gmail.com>
2021-10-09 22:06:35 +0200

    week 5 homework

---
+++ week5/predict.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import pickle
+
+model_file = "model1.bin"
+dv_file = "dv.bin"
+
+with open(model_file, 'rb') as f_in:
+    model = pickle.load(f_in)
+
+with open(dv_file, "rb") as f_in:
+    dv = pickle.load(f_in)
+
+app = Flask("churn")
+
+@app.route("/predict", methods = ["POST"])
+
+def predict():
+    customer = request.get_json()
+    y_pred = use_model(dv, model, customer)
+    churn = y_pred >= 0.5
+    result = {
+        "churn_probability": float(y_pred), 
+        "churn": bool(churn)
+    }
+    return jsonify(result)
+
+def use_model(dv, model, customer):
+	X = dv.transform([customer])
+	return model.predict_proba(X)[0,1]
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
